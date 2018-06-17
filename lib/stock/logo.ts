07647fae773d32810bc8d8ee1e0f9d3133b87d1a
Jonathan Gawrych <Jonathan.Gawrych@gmail.com>
2018-06-17 12:54:37 -0700

    Implement Logo

---
+++ lib/stock/logo.ts
@@ -0,0 +1,14 @@
+// https://iextrading.com/developer/docs/#logo
+// This is a helper function, but the google APIs url is standardized.
+export namespace Logo {
+	export const path = (symbol:string) => `/stock/{symbol}/logo`;
+
+	export interface Response {
+		url: string,
+		[futureProp: string]: any
+	}
+
+	export function get(symbol: string): Promise<Response> {
+		return fetch(path(symbol)).then(res => res.json());
+	}
+}
