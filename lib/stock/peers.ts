4aa31182b363c044e811bd8a1e00f4c1736414e4
Jonathan Gawrych <Jonathan.Gawrych@gmail.com>
2018-06-17 13:33:20 -0700

    Implement Peers

---
+++ lib/stock/peers.ts
@@ -0,0 +1,16 @@
+// https://iextrading.com/developer/docs/#peers
+export namespace Peers {
+	export const path = (symbol:string) => `/stock/{symbol}/peers`;
+
+	/**
+	 * An array of peer tickers as defined by IEX. This is not intended to represent
+	 * a definitive or accurate list of peers, and is subject to change at any time.
+	 */
+	export interface Response {
+		[index: number]: string;
+	}
+
+	export function get(symbol: string): Promise<Response> {
+		return fetch(path(symbol)).then(res => res.json());
+	}
+}
