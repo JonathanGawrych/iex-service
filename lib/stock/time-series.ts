23f075cdf647d1f89dfddeaaf1d04f9f415cf0f4
Jonathan Gawrych <Jonathan.Gawrych@gmail.com>
2018-06-17 16:08:01 -0700

    Implement TimeSeries

---
+++ lib/stock/time-series.ts
@@ -0,0 +1,4 @@
+// https://iextrading.com/developer/docs/#time-series
+// time-series is an alternate way to access the chart endpoint.
+// Note this doesn't actually use the "time-series" path, but that should be fine.
+export { Chart as TimeSeries } from './chart'
