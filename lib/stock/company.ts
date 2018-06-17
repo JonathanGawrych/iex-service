d0288c39cdd7e6a72b7d58b1decddb927f1dc929
Jonathan Gawrych <Jonathan.Gawrych@gmail.com>
2018-06-17 12:00:02 -0700

    Implement Company
    Note: IEX's API is missing property documentation

---
+++ lib/stock/company.ts
@@ -0,0 +1,32 @@
+// https://iextrading.com/developer/docs/#company
+export namespace Company {
+	export const path = (symbol:string) => `/stock/{symbol}/company`;
+
+	// The common issue type of the stock.
+	// ad – American Depository Receipt (ADR’s)
+	// re – Real Estate Investment Trust (REIT’s)
+	// ce – Closed end fund (Stock and Bond Fund)
+	// si – Secondary Issue
+	// lp – Limited Partnerships
+	// cs – Common Stock
+	// et – Exchange Traded Fund (ETF)
+	// (blank) = Not Available, i.e., Warrant, Note, or (non-filing) Closed Ended Funds
+	export type IssueType = 'ad' | 're' | 'ce' | 'si' | 'lp' | 'cs' | 'et' | '';
+
+	export interface Response {
+		symbol: string,
+		companyName: string,
+		exchange: string,
+		industry: string,
+		website: string,
+		description: string,
+		CEO: string,
+		issueType: IssueType,
+		sector: string,
+		[futureProp: string]: any
+	}
+
+	export function get(symbol: string): Promise<Response> {
+		return fetch(path(symbol)).then(res => res.json());
+	}
+}
