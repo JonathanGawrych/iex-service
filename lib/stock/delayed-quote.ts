64ad006a74a853380af052722fc86d9fe45752d1
Jonathan Gawrych <Jonathan.Gawrych@gmail.com>
2018-06-17 12:10:29 -0700

    Implement DelayedQuote

---
+++ lib/stock/delayed-quote.ts
@@ -0,0 +1,18 @@
+// https://iextrading.com/developer/docs/#delayed-quote
+// The 15 minute delayed market quote.
+export namespace DelayedQuote {
+	export const path = (symbol:string) => `/stock/{symbol}/delayed-quote`;
+
+	export interface Response {
+		symbol: string,           // The stock ticker
+		delayedPrice: number,     // The 15 minute delayed market price
+		delayedSize: number,      // The 15 minute delayed last trade size
+		delayedPriceTime: number, // The time of the delayed market price
+		processedTime: number,    // The time when IEX processed the SIP price
+		[futureProp: string]: any
+	}
+
+	export function get(symbol: string): Promise<Response> {
+		return fetch(path(symbol)).then(res => res.json());
+	}
+}
