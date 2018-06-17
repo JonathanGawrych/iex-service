b70139789d5ffae2190e74d77ae6d4429a1f97c6
Jonathan Gawrych <Jonathan.Gawrych@gmail.com>
2018-06-17 13:50:11 -0700

    Implement Price

---
+++ lib/stock/price.ts
@@ -0,0 +1,14 @@
+// https://iextrading.com/developer/docs/#price
+/**
+ * A single number, being the IEX real time price, the 15 minute delayed market price,
+ * or the previous close price, is returned.
+ */
+export namespace Price {
+	export const path = (symbol:string) => `/stock/{symbol}/price`;
+
+	export type Response = number;
+
+	export function get(symbol: string): Promise<Response> {
+		return fetch(path(symbol)).then(res => res.text()).then(priceStr => Number.parseInt(priceStr, 10));
+	}
+}
