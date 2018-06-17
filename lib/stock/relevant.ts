748b057a901aa0acbb657cacb175b33b934957e6
Jonathan Gawrych <Jonathan.Gawrych@gmail.com>
2018-06-17 14:26:14 -0700

    Implement Relevant

---
+++ lib/stock/relevant.ts
@@ -0,0 +1,19 @@
+// https://iextrading.com/developer/docs/#relevant
+/**
+ * Similar to the peers endpoint, except this will return most active market symbols when
+ * peers are not available. If the symbols returned are not peers, the peers key will be false.
+ * This is not intended to represent a definitive or accurate list of peers, and is subject to change at any time.
+ */
+export namespace Relevant {
+	export const path = (symbol:string) => `/stock/{symbol}/relevant`;
+
+	export interface Response {
+		peers: boolean,
+		symbols: string[],
+		[futureProp: string]: any
+	}
+
+	export function get(symbol: string): Promise<Response> {
+		return fetch(path(symbol)).then(res => res.json());
+	}
+}
