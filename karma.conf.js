6f0a5b5089958fd03449f570db3bfa1a29c9ac3e
Jonathan Gawrych <Jonathan.Gawrych@gmail.com>
2018-06-18 00:50:23 -0700

    Move tests directly into the lib directory
    Files in a test directory move the entire destination folder structure,
    which then breaks all relatively pathing from base.

--- karma.conf.js
+++ karma.conf.js
@@ -15,8 +15,7 @@ module.exports = function(config) {
 
 		// list of files / patterns to load in the browser
 		files: [
-			'lib/**/*.ts',
-			'test/**/*.spec.ts'
+			'lib/**/*.ts'
 		],
 
 		// list of files / patterns to exclude
--- lib/stock/stock.spec.ts
+++ lib/stock/stock.spec.ts
@@ -1,5 +1,5 @@
 import * as fetchMock from 'fetch-mock';
-import { Stock } from '../lib';
+import * as Stock from './index';
 
 describe('Stock', () => {
 	afterEach(() => {
